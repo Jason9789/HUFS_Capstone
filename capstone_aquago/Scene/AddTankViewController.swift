14d1e59851f6daac2feafd2d51a4aaa86f5ba3ec
Jason9789 <lifebook0809@gmail.com>
2021-05-15 16:26:39 +0900

    feat: Add TankViewcontroller

--- capstone_aquago/Scene/AddTankViewController.swift
+++ capstone_aquago/Scene/AddTankViewController.swift
@@ -13,6 +13,9 @@ import RxCocoa
 class AddTankViewcontroller: UIViewController {
     
     //MARK:- Private
+    
+    private let bag = DisposeBag()
+    
     private let labelTitle: UILabel = {
         let lb = UILabel()
         lb.text = "수조 추가"
@@ -31,20 +34,81 @@ class AddTankViewcontroller: UIViewController {
         return v
     }()
     
+    private let labelWaterTank: UILabel = {
+        let lb = UILabel()
+        lb.text = "수조 이름"
+        lb.textColor = UIColor(red: 78/255, green: 148/255, blue: 199/255, alpha: 1)
+        lb.font = UIFont.boldSystemFont(ofSize: 15)
+        return lb
+    }()
+    
+    private let labelFish: UILabel = {
+        let lb = UILabel()
+        lb.text = "어종"
+        lb.textColor = UIColor(red: 78/255, green: 148/255, blue: 199/255, alpha: 1)
+        lb.font = UIFont.boldSystemFont(ofSize: 15)
+        return lb
+    }()
+    
+    private let tfWaterTank : UITextField = {
+        let tf = UITextField()
+        tf.addLeftPadding()
+        tf.placeholder = "수조 이름"
+        tf.layer.borderWidth = 2.0
+        tf.layer.cornerRadius = 10.0
+        tf.layer.borderColor = CGColor(red: 78/255, green: 148/255, blue: 199/255, alpha: 1.0)
+        
+        return tf
+    }()
+    
+    private let tfFish : UITextField = {
+        let tf = UITextField()
+        tf.addLeftPadding()
+        tf.placeholder = "어종"
+        tf.layer.borderWidth = 2.0
+        tf.layer.cornerRadius = 10.0
+        tf.layer.borderColor = CGColor(red: 78/255, green: 148/255, blue: 199/255, alpha: 1.0)
+        
+        return tf
+    }()
+    
+    private let btnAdd: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("추가하기", for: .normal)
+        btn.setTitleColor(.white, for: .normal)
+        btn.backgroundColor = UIColor(red: 78/255, green: 148/255, blue: 199/255, alpha: 1.0)
+        btn.layer.cornerRadius = 10.0
+        
+        return btn
+    }()
+    
     //MARK:- Lifecycle
     override func viewDidLoad() {
-        print("TANK")
+
         configureView()
         configureSubView()
+        bindRx()
     }
     
+    
+    //MARK:- ConfigureView
     private func configureView() {
         self.view.backgroundColor = .white
         
         view.addSubview(labelTitle)
         view.addSubview(lineView)
+        
+        view.addSubview(labelWaterTank)
+        view.addSubview(tfWaterTank)
+        
+        view.addSubview(labelFish)
+        view.addSubview(tfFish)
+        
+        view.addSubview(btnAdd)
     }
     
+    
+    //MARK:- configureSubView
     private func configureSubView() {
         
         labelTitle.snp.makeConstraints {
@@ -58,5 +122,47 @@ class AddTankViewcontroller: UIViewController {
             $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-60)
         }
         
+        labelWaterTank.snp.makeConstraints {
+            $0.top.equalTo(lineView.snp.bottom).offset(56)
+            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(16)
+        }
+        
+        tfWaterTank.snp.makeConstraints {
+            $0.top.equalTo(labelWaterTank.snp.bottom).offset(16)
+            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(16)
+            $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-16)
+            $0.height.equalTo(45)
+        }
+        
+        labelFish.snp.makeConstraints {
+            $0.top.equalTo(tfWaterTank.snp.bottom).offset(100)
+            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(16)
+        }
+        
+        tfFish.snp.makeConstraints {
+            $0.top.equalTo(labelFish.snp.bottom).offset(16)
+            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(16)
+            $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-16)
+            $0.height.equalTo(45)
+        }
+        
+        btnAdd.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-40)
+            $0.width.equalTo(220)
+            $0.height.equalTo(50)
+        }
+        
+    }
+    
+    
+    //MARK:- BindRx
+    private func bindRx() {
+        btnAdd.rx
+            .tap
+            .bind {
+                print("추가하기")
+                self.dismiss(animated: true, completion: nil)
+            }.disposed(by: bag)
     }
 }
